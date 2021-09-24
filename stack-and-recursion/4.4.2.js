6406bb58ba667fa122afd2ca86c08a480cbaf66b
logan <gnlee95@gmail.com>
2021-09-24 22:04:28 +0900

    Update question 4.4.2 permutation

--- stack-and-recursion/4.4.2.js
+++ stack-and-recursion/4.4.2.js
@@ -1,16 +1,18 @@
 const input = 'abcd';
 const inputArray = input.split('');
 
-const getPermutation = (inputArray) => {
-  let result = [];
-  if (inputArray.length === 1) return [inputArray];
+const getPermutation = (array) => {
+  const result = [];
+  if (array.length === 1) return [array];
 
-  for (let pickedLetter of inputArray) {
-    const temp = [...inputArray];
+  for (const pickedLetter of array) {
+    let temp = [...array];
     temp.splice(temp.indexOf(pickedLetter), 1);
     const nonPickedArr = temp;
     const permutation = getPermutation(nonPickedArr);
-    const sumPickedLetter = permutation.map((e) => e.unshift(pickedLetter));
+    const sumPickedLetter = permutation.map((e) => {
+      e.push(pickedLetter);
+    });
     result.push(sumPickedLetter);
   }
   return result;
