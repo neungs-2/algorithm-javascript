02a3aa7d993d40c39e4fa84f6ff76dab5da419c1
logan <gnlee95@gmail.com>
2021-10-09 12:36:12 +0900

    Update solve 신규 아이디 추천 of kakao

--- programmers/신규_아이디_추천.js
+++ programmers/신규_아이디_추천.js
@@ -1,3 +1,21 @@
-var a = 'UuPpTtOo';
-a = a.toLowerCase();
-console.log(a);
+function solution(new_id) {
+  var answer = '';
+
+  answer = new_id
+    .toLowerCase()
+    .replace(/[^\w\-_.]/gi, '')
+    .replace(/\.+/g, '.')
+    .replace(/^\.|\.$/g, '')
+    .replace(/^$/, 'a')
+    .substr(0, 15)
+    .replace(/^\.|\.$/g, '');
+
+  while (answer.length < 3) {
+    answer += answer.charAt(answer.length - 1);
+  }
+
+  return answer;
+}
+
+const result = solution('...!@BaT#*..y.abcdefghijklm');
+console.log(result);
