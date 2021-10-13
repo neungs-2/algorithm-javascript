36342f4ca97a1e504b12178767a501438e94e379
logan <gnlee95@gmail.com>
2021-10-13 22:48:02 +0900

    Update solve bubble sort

--- sort/bubble_sort.js
+++ sort/bubble_sort.js
@@ -1,8 +1,6 @@
 function bubbleSort(arr = []) {
   for (i = arr.length; i > 0; i--) {
     for (j = 0; j < i - 1; j++) {
-      // let [front, back] = [arr[j], arr[j + 1]];
-
       if (arr[j] > arr[j + 1]) {
         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
       }
