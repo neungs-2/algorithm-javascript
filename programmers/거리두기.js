d874f3737a930d5e9088297067d5236ec973f5b9
logan <gnlee95@gmail.com>
2021-11-07 12:10:47 +0900

    Add question of kakao

---
+++ programmers/거리두기.js
@@ -0,0 +1,32 @@
+const x = [
+  ['POOOP', 'OXXOX', 'OPXPX', 'OOXOX', 'POXXP'],
+  ['POOPX', 'OXPXP', 'PXXXO', 'OXXXO', 'OOOPP'],
+  ['PXOPX', 'OXOXP', 'OXPOX', 'OXXOP', 'PXPOX'],
+  ['OOOXX', 'XOOOX', 'OOOXX', 'OXOOX', 'OOOOO'],
+  ['PXPXP', 'XPXPX', 'PXPXP', 'XPXPX', 'PXPXP'],
+];
+
+function solution(places) {
+  const answer = [];
+  const location = [];
+  let status = 0;
+
+  for (let room of places) {
+    
+  }
+
+  const findPerson = (arr) => {
+    const 
+    let row = 0;
+
+    for (let i of arr) {
+      const rowArr = i.split('');
+      row
+    }
+
+  }
+
+  return room;
+}
+
+solution(x);
