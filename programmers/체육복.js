a3840ce4110ea314eff4522eccb643368a956e63
logan <gnlee95@gmail.com>
2021-11-25 02:14:28 +0900

    Update greedy algorithm question

--- programmers/체육복.js
+++ programmers/체육복.js
@@ -1,5 +1,7 @@
 function solution(n, lost, reserve) {
   var answer = 0;
+  lost.sort((a, b) => a - b);
+  reserve.sort((a, b) => a - b);
 
   // reserve 학생 중 lost 포함은 두 Arr에서 제외
   const reserveButLost = reserve.filter((student) => lost.includes(student));
@@ -7,13 +9,13 @@ function solution(n, lost, reserve) {
   lost = lost.filter((student) => !reserveButLost.includes(student));
 
   // lost 학생 Loop 돌며 -+1 번호 reserve 존재 확인 후 pop
-  for (lostStudent of lost) {
-    if (lostStudent - 1 in reserve) {
+  for (let lostStudent of [...lost]) {
+    if (reserve.includes(lostStudent - 1)) {
       lost.splice(lost.indexOf(lostStudent), 1);
-      reserve.splice(reserve.indexOf(lostStudent - 1, 1));
-    } else if (lostStudent + 1 in reserve) {
+      reserve.splice(reserve.indexOf(lostStudent - 1), 1);
+    } else if (reserve.includes(lostStudent + 1)) {
       lost.splice(lost.indexOf(lostStudent), 1);
-      reserve.splice(reserve.indexOf(lostStudent + 1, 1));
+      reserve.splice(reserve.indexOf(lostStudent + 1), 1);
     }
   }
 
