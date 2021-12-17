c2a2da1d6dc24a666b49b10325f965aeabffcba9
logan <gnlee95@gmail.com>
2021-12-17 19:03:12 +0900

    Update incorrect solution

--- programmers/level1/키패드_누르기.js
+++ programmers/level1/키패드_누르기.js
@@ -1,9 +1,17 @@
 function solution(numbers, hand) {
-  const keypad = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-  ];
+  const answer = [];
+  const keypad = {
+    1: [0, 0],
+    2: [0, 1],
+    3: [0, 2],
+    4: [1, 0],
+    5: [1, 1],
+    6: [1, 2],
+    7: [2, 0],
+    8: [2, 1],
+    9: [2, 2],
+    0: [3, 1],
+  };
 
   const fixedAnswer = {
     1: 'L',
@@ -15,10 +23,39 @@ function solution(numbers, hand) {
   };
 
   const location = {
-    left: [3, 0],
-    right: [3, 2],
+    L: [3, 0],
+    R: [3, 2],
   };
-  console.log(keypad[0][2]);
+
+  for (let num of numbers) {
+    if (Object.keys(fixedAnswer).includes(num)) {
+      answer.push(fixedAnswer[num]);
+      location[fixedAnswer[num]] = keypad[fixedAnswer[num]];
+      continue;
+    }
+
+    const numLocation = keypad[num];
+    const leftToNum =
+      (numLocation[0] - location['L'][0]) ** 2 +
+      (numLocation[1] - location['L'][1]) ** 2;
+    const rightToNum =
+      (numLocation[0] - location['R'][0]) ** 2 +
+      (numLocation[1] - location['R'][1]) ** 2;
+
+    if (leftToNum < rightToNum) {
+      answer.push('L');
+      location['L'] = numLocation;
+    } else if (rightToNum < leftToNum) {
+      answer.push('R');
+      location['R'] = numLocation;
+    } else {
+      answer.push(hand.toUpperCase()[0]);
+      location[hand.toUpperCase()[0]] = numLocation;
+    }
+  }
+
+  return answer.join('');
 }
 
-solution();
+const sol = solution([1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5], 'right');
+console.log(sol);
