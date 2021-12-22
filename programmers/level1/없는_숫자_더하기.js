45db35404bf83a2a6189eb36529e6423d0f7e6b7
logan <gnlee95@gmail.com>
2021-12-23 01:54:13 +0900

    Solve the question

--- programmers/level1/없는_숫자_더하기.js
+++ programmers/level1/없는_숫자_더하기.js
@@ -1,4 +1,5 @@
-function solution(numbers) {
-  var answer = -1;
-  return answer;
-}
+const zeroToNine = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const solution = (numbers) =>
+  zeroToNine
+    .filter((num) => !numbers.includes(num))
+    .reduce((pre, cur) => pre + cur, 0);
