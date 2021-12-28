8f4e5a2b5d022b50875fe9b8d80b4b9a10ac51d4
logan <gnlee95@gmail.com>
2021-12-28 23:29:59 +0900

    Solve the question

--- programmers/coding-test/level1/실패율.js
+++ programmers/coding-test/level1/실패율.js
@@ -1,23 +1,39 @@
 const solution = (N, stages) => {
   const failureRate = [];
+  const answer = [];
 
   for (let i = 1; i <= N; i++) {
     const stoppedPlayers = stages.filter((stage) => stage === i).length;
     const reachedPlayers = stages.filter((stage) => stage >= i).length;
-    failureRate.push(stoppedPlayers / reachedPlayers);
+    const rate = reachedPlayers !== 0 ? stoppedPlayers / reachedPlayers : 0;
+    failureRate.push(rate);
   }
 
-  const failureObject = Object.entries({ ...failureRate }).reduce(
-    (obj, [key, val]) => ({ ...obj, [val]: key }),
-    {}
-  );
-  console.log(failureObject, { ...failureRate });
-  failureRate.sort((a, b) => b - a);
+  const getKeyByValue = (object, value) => {
+    return Object.keys(object).find((key) => object[key] === value);
+  };
 
-  return failureRate.reduce(
-    (answer, rate) => answer.push(failureObject[rate]),
-    []
-  );
+  const failureObject = { ...failureRate };
+  failureRate
+    .sort((a, b) => b - a)
+    .forEach((rate) => {
+      const stage = getKeyByValue(failureObject, rate);
+      answer.push(1 + Number(stage));
+      failureObject[stage] = 'already used';
+    });
+
+  return answer;
 };
 
-console.log(solution(4, [4, 4, 4, 4, 4]));
+// 2차원 배열 사용한 답안
+function solution(N, stages) {
+  let result = [];
+  for (let i = 1; i <= N; i++) {
+    let reach = stages.filter((x) => x >= i).length;
+    let curr = stages.filter((x) => x === i).length;
+    result.push([i, curr / reach]);
+  }
+  result.sort((a, b) => b[1] - a[1]);
+
+  return result.map((x) => x[0]);
+}
