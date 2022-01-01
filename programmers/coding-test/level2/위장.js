f8c30dc8e182eb7040d3c0655acf10244becf8c5
logan <gnlee95@gmail.com>
2022-01-02 00:49:44 +0900

    Refactor answer

--- programmers/coding-test/level2/위장.js
+++ programmers/coding-test/level2/위장.js
@@ -1,42 +1,11 @@
 function solution(clothes) {
-  let answer = 0;
-  const clothesNumArr = numClothesOfType(clothes);
-
-  for (let numOfType = 1; numOfType <= clothes.length; numOfType++) {
-    const cases = combination(clothesNumArr, numOfType);
-    for (let i = 0; i < cases.length; i++) {
-      const numOfCases = cases[i].reduce((pre, curr) => (pre *= curr), 1);
-      answer += numOfCases;
-    }
-  }
-
-  return answer;
-}
-
-function numClothesOfType(matrix) {
-  const type = {};
-
-  matrix.forEach((arr) =>
-    Object.keys(type).includes(arr[1]) ? ++type[arr[1]] : (type[arr[1]] = 1)
+  const clothesNumObj = {};
+  clothes.forEach(
+    (arr) => (clothesNumObj[arr[1]] = (clothesNumObj[arr[1]] || 1) + 1)
   );
-  return Object.values(type);
-}
 
-function combination(arr, selectNum) {
-  const result = [];
-
-  if (selectNum === 1) return arr.map((v) => [v]);
-
-  arr.forEach((val, idx, arr) => {
-    const fixed = val;
-    const restArr = arr.slice(idx + 1);
-    const combinationArr = combination(restArr, selectNum - 1);
-    const combineFix = combinationArr.map((v) => [fixed, ...v]);
-
-    result.push(...combineFix);
-  });
-
-  return result;
+  const clothesNumArr = [...Object.values(clothesNumObj)];
+  return clothesNumArr.reduce((pre, curr) => pre * curr, 1) - 1;
 }
 
 const m = [
