602b551173bb32b24c6c4e27f46f5bb96f0ba451
logan <gnlee95@gmail.com>
2022-01-01 23:56:50 +0900

    Solve the question

--- programmers/coding-test/level2/위장.js
+++ programmers/coding-test/level2/위장.js
@@ -1,28 +1,6 @@
 function solution(clothes) {
   let answer = 0;
-
-  //TODO: 처음 문제 조건 잘 못 읽음 --> clothesNumber 함수 수정하기
-  const clothesNumArr = clothes.reduce(
-    (numOfClothes, curr) => [...numOfClothes, curr.length],
-    []
-  );
-
-  function combination(arr, selectNum) {
-    const result = [];
-
-    if (selectNum === 1) return arr.map((v) => [v]);
-
-    arr.forEach((val, idx, arr) => {
-      const fixed = val;
-      const restArr = arr.slice(idx + 1);
-      const combinationArr = combination(restArr, selectNum - 1);
-      const combineFix = combinationArr.map((v) => [fixed, ...v]);
-
-      result.push(...combineFix);
-    });
-
-    return result;
-  }
+  const clothesNumArr = numClothesOfType(clothes);
 
   for (let numOfType = 1; numOfType <= clothes.length; numOfType++) {
     const cases = combination(clothesNumArr, numOfType);
@@ -35,10 +13,36 @@ function solution(clothes) {
   return answer;
 }
 
-console.log(
-  solution([
-    ['yellowhat', 'headgear'],
-    ['bluesunglasses', 'eyewear'],
-    ['green_turban', 'headgear'],
-  ])
-);
+function numClothesOfType(matrix) {
+  const type = {};
+
+  matrix.forEach((arr) =>
+    Object.keys(type).includes(arr[1]) ? ++type[arr[1]] : (type[arr[1]] = 1)
+  );
+  return Object.values(type);
+}
+
+function combination(arr, selectNum) {
+  const result = [];
+
+  if (selectNum === 1) return arr.map((v) => [v]);
+
+  arr.forEach((val, idx, arr) => {
+    const fixed = val;
+    const restArr = arr.slice(idx + 1);
+    const combinationArr = combination(restArr, selectNum - 1);
+    const combineFix = combinationArr.map((v) => [fixed, ...v]);
+
+    result.push(...combineFix);
+  });
+
+  return result;
+}
+
+const m = [
+  ['yellowhat', 'headgear'],
+  ['bluesunglasses', 'eyewear'],
+  ['green_turban', 'headgear'],
+];
+
+console.log(solution(m));
