fe17c9f303faa69235f7b6e7f9deda4974c64ece
logan <gnlee95@gmail.com>
2022-01-07 11:53:10 +0900

    Add appendLast, delete and retrieve method

--- data-structure/linked-list/one-way_linked_list.js
+++ data-structure/linked-list/one-way_linked_list.js
@@ -11,14 +11,50 @@ class LinkedList {
     this.size++;
   }
 
-  append(data) {
+  appendLast(data) {
     if (!this.head) {
       this.appendHead(data);
     } else {
+      let cursor = this.head;
+      while (cursor.next) {
+        cursor = cursor.next;
+      }
+      cursor.next = new Node(data);
+      this.size++;
     }
   }
 
-  delete() {}
+  appendAt(data, index) {
+    let count = 0;
 
-  retrieve() {}
+    while (count === index) {}
+  }
+
+  delete(data) {
+    if (this.head.data === data) {
+      this.head = this.head.next;
+      this.size--;
+      return this;
+    }
+
+    let cursor = this.head.next;
+    let front = this.head;
+    let back = this.head.next.next;
+
+    while (cursor.next) {
+      if (cursor.data === data) {
+        front.next = back;
+      }
+    }
+    this.size--;
+  }
+
+  retrieve() {
+    let cursor = this.head;
+
+    while (cursor) {
+      console.log(cursor.data);
+      cursor = cursor.next;
+    }
+  }
 }
