2629a4d55e757f6c874176970084d241e15c15c6
logan <gnlee95@gmail.com>
2022-01-11 09:33:10 +0900

    Solve the question

--- programmers/coding-test/level2/가장큰수.js
+++ programmers/coding-test/level2/가장큰수.js
@@ -1,22 +1,8 @@
 function solution(numbers) {
-  let answer = '';
-  const fourDigitObj = {};
-  const fourDigitNumArr = numbers.map((num, idx) => {
-    let stringNum = '' + num;
-    while (stringNum.length < 4) {
-      stringNum += stringNum;
-    }
-    const fourDigitNum = stringNum.slice(0, 4);
-    fourDigitObj[fourDigitNum] = idx;
-    return fourDigitNum;
-  });
+  let answer = numbers
+    .map((num) => String(num))
+    .sort((a, b) => b + a - (a + b))
+    .join('');
 
-  fourDigitNumArr.sort((a, b) => b - a);
-  fourDigitNumArr.forEach((val) => (answer += numbers[fourDigitObj[val]]));
-
-  if (answer[0] === '0') return 0;
-  return answer;
+  return answer[0] === '0' ? '0' : answer;
 }
-
-const sol = solution([0, 0, 0, 0]);
-console.log(sol);
