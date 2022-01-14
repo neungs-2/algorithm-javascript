de036b36fdf7a84d556e5e27cae2d5e3ff9f356b
logan <gnlee95@gmail.com>
2022-01-14 14:18:02 +0900

    Solve the question

---
+++ js-algorithm-problem-solving/section1/최솟값_구하기.js
@@ -0,0 +1,15 @@
+// 7개의 수가 주어지면 그 숫자 중 가장 작은 수를 출력하는 프로그램을 작성하세요.
+function solution(...args) {
+  return Math.min(...args);
+}
+
+// Answer Sheet
+function solution(arr) {
+  let answer,
+    min = Number.MAX_SAFE_INTEGER;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) min = arr[i];
+  }
+  answer = min;
+  return answer;
+}
