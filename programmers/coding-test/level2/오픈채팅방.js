5afe173570b4917acc7c299bcbd15235870cf8dc
logan <gnlee95@gmail.com>
2022-02-11 16:49:13 +0900

    Refactor code

--- programmers/coding-test/level2/오픈채팅방.js
+++ programmers/coding-test/level2/오픈채팅방.js
@@ -22,8 +22,27 @@ function solution(record) {
   return answer;
 }
 
+// Refactoring - 코드 가독성을 향상
+function solution2(record) {
+  const userObj = {};
+  const action = [];
+  const comment = { Enter: '님이 들어왔습니다.', Leave: '님이 나갔습니다.' };
+
+  record.forEach((log) => {
+    const [state, id, nick] = log.split(' ');
+
+    if (state !== 'Change') action.push([state, id]);
+
+    if (state === 'Enter' || state === 'Change') {
+      userObj[id] = nick;
+    }
+  });
+
+  return action.map(([state, id]) => `${userObj[id]}${comment[state]}`);
+}
+
 console.log(
-  solution([
+  solution2([
     'Enter uid1234 Muzi',
     'Enter uid4567 Prodo',
     'Leave uid1234',
