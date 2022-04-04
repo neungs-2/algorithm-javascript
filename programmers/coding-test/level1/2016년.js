a030836b875736742fa8925a0c6ec8adb689dc13
logan <gnlee95@gmail.com>
2022-04-05 00:09:28 +0900

    Refactor logic and data structure

--- programmers/coding-test/level1/2016년.js
+++ programmers/coding-test/level1/2016년.js
@@ -1,23 +1,17 @@
 const solution = (a, b) => {
-  const dayObject = {
-    1: 'FRI',
-    2: 'SUN',
-    3: 'SAT',
-    4: 'MON',
-    5: 'TUE',
-    6: 'WED',
-    7: 'THU',
-  };
-
+  const dayObject = ['THU', 'FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED'];
   const monthlyNumOfDate = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  let totalDate = b;
 
-  const totalDate = monthlyNumOfDate.reduce(
-    (pre, cur, idx) =>
-      idx < a - 1 ? pre + cur : idx === a - 1 ? pre + b : pre,
-    0
-  );
+  for (let i = 0; i < a - 1; i++) {
+    totalDate += monthlyNumOfDate[i];
+  }
+
+  // const totalDate = monthlyNumOfDate.reduce(
+  //   (pre, cur, idx) =>
+  //     idx < a - 1 ? pre + cur : idx === a - 1 ? pre + b : pre,
+  //   0
+  // );
 
   return dayObject[totalDate % 7];
 };
-
-console.log(solution(3, 4));
