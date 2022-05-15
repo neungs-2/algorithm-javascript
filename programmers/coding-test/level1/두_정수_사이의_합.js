42085de724823e932cf2d426744fa5bb114aad62
logan <gnlee95@gmail.com>
2022-05-16 00:12:56 +0900

    Solve the question other ways

--- programmers/coding-test/level1/두_정수_사이의_합.js
+++ programmers/coding-test/level1/두_정수_사이의_합.js
@@ -6,4 +6,32 @@ const solution = (a, b) =>
         (v, i) => Math.min(a, b) + i
       ).reduce((pre, cur) => pre + cur, 0);
 
-console.log(solution(3, 5));
+const solution2 = (a, b) => {
+  if (a === b) return a;
+  return Array.from(
+    { length: Math.abs(a - b) + 1 },
+    (v, i) => Math.min(a, b) + i
+  ).reduce((pre, cur) => pre + cur, 0);
+};
+
+function solution3(a, b) {
+  if (a === b) return a;
+  return Array.from(
+    { length: Math.abs(a - b) + 1 },
+    (v, i) => Math.min(a, b) + i
+  ).reduce((pre, cur) => pre + cur, 0);
+}
+
+// 제일 성능 좋은 코드
+// 위의 로직은 reduce를 사용하기 위해 Array를 만드는 과정에서 시간소요가 큰 듯.
+// 어차피 reduce도 내부에서 루프가 돌기 때문에 루프를 배제하기 위해 사용할 필요는 없을 듯.
+function solution4(a, b) {
+  if (a === b) return a;
+  let answer = 0;
+
+  for (let i = Math.min(a, b); i <= Math.max(a, b); i++) {
+    answer += i;
+  }
+
+  return answer;
+}
