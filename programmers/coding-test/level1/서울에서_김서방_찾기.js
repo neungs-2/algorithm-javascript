6887426919e514561c40fe69365e168d3bad4651
logan <gnlee95@gmail.com>
2022-06-06 23:53:06 +0900

    Solve the question

---
+++ programmers/coding-test/level1/서울에서_김서방_찾기.js
@@ -0,0 +1,7 @@
+function solution(seoul) {
+  const index = seoul.indexOf('Kim');
+  return `김서방은 ${index}에 있다`;
+}
+
+// 함수 길이 줄인 풀이
+const solution2 = (seoul) => `김서방은 ${seoul.indexOf('Kim')}에 있다`;
